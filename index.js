e3a06780bf87ef16db484b88bf1ab477daacf226
Bordács Béla <side1111@hotmail.com>
2024-09-20 17:51:39 +0200

    alakitas az index.js-en

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ import { Kartyak } from "./Kartyak.js";
 
 const kartyak = adatlista.map(adat => new Kartya(adat.id, adat.url, adat.name, adat.leiras));
 
-// Kartyak osztály inicializálása
+
 const kartyakObj = new Kartyak(kartyak);
 
-// A HTML elemek lekérése
+
 const pictureElem = document.querySelector('.picture');
 const carouselElem = document.querySelector('.carousel');
 
-// Kezdő kép és thumbnail-ek megjelenítése
+
 kartyakObj.displayCurrentPicture(pictureElem);
 kartyakObj.displayThumbnails(carouselElem);
 
-// Következő és előző gombok eseménykezelése
+
 document.querySelector('.next').addEventListener('click', () => {
     kartyakObj.nextPicture();
     kartyakObj.displayCurrentPicture(pictureElem);
